99fb803ef4e670dcc2a8086a95e7730ebd72e4e6
Hugh Hartigan <Hartigan.HM@gmail.com>
2019-12-14 13:28:41 -0700

    Build types

---
+++ src/types.ts
@@ -0,0 +1,49 @@
+// NAVIGATION
+export interface CurrentlyViewingTab  {
+  label: string;
+  path: string;
+  disabled: boolean;
+}
+
+export interface CurrentlyViewing {
+  path: string;
+  title: string;
+  subTitle?: string;
+  backPath?: string;
+  backTitle?: string;
+  tabs?: {
+    currentTab: string;
+    tabs: CurrentlyViewingTab[];
+  };
+}
+
+export interface HeaderMenuOptions {
+  [key: string]: HeaderMenuOption
+}
+
+export interface HeaderMenuOption {
+  icon: string;
+  subTitle?: string;
+  subOptions?: HeaderMenuSubOption[];
+  path?: string;
+  indicator?: number | boolean;
+  isActive?: boolean;
+}
+
+export interface HeaderMenuSubOption {
+  icon: string;
+  label: string;
+  hasAccess: boolean;
+  path?: string;
+  href?: string;
+}
+
+export interface SideNavigationOptions {
+  [key: string]: {
+    icon: string;
+    subOptions: string[];
+    titleType: string;
+    // titleType: 'option' | 'subOption' | 'option subOption' | 'subOption option';
+  }
+}
+
