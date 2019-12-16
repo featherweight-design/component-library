8102cf059ca4eb40d47254c6292b2ebb33860c38
Hugh Hartigan <Hartigan.HM@gmail.com>
2019-12-15 21:14:00 -0700

    Migrate index.ts file

--- src/components/index.ts
+++
@@ -1,6 +0,0 @@
-import Button from './Button/Button';
-import { CircleLoader } from './Loaders';
-import ExpansionPanel from './ExpansionPanel/ExpansionPanel';
-import { HeaderMenu, SideNavigation } from './Navigation';
-
-export { CircleLoader, ExpansionPanel, HeaderMenu, SideNavigation };
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import Button from './components/Button/Button';
+import { CircleLoader } from './components/Loaders';
+import ExpansionPanel from './components/ExpansionPanel/ExpansionPanel';
+import { HeaderMenu, SideNavigation } from './components/Navigation';
+
+export { CircleLoader, ExpansionPanel, HeaderMenu, SideNavigation };
