f1cc83260db2eb76461c65c2a5800da04df06061
Hugh Hartigan <Hartigan.HM@gmail.com>
2019-12-21 11:31:56 -0700

    Build types/classNames for Button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,13 +1,22 @@
 import React, { ReactChild } from 'react';
+import classnames from 'classnames';
 
 import './Button.scss';
 
 type ButtonProps = {
   children: string | number | ReactChild | ReactChild[];
+  type?: ButtonType;
+  className?: string;
 }
 
-const Button = ({ children }: ButtonProps) => (
-  <button className="fd-button">
+type ButtonType = 'default-destructive' | 'brand' | 'neutral' | 'outline' | 'destructive' | 'outline-destructive';
+
+const Button = ({ children, type, className }: ButtonProps) => (
+  <button className={classnames({
+    'fd-button': true,
+    [`fd-button-${type}`]: type,
+    [className as string]: className
+  })}>
     {children}
   </button>
 )
