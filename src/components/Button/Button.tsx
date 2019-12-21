fecf533d778655de971e6e1a551ea509fb9a37d6
Hugh Hartigan <Hartigan.HM@gmail.com>
2019-12-21 12:26:38 -0700

    Fix linting in Button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -11,22 +11,41 @@ type ButtonProps = {
   className?: string;
   disabled?: boolean;
   loading?: boolean;
-}
+};
 
-type ButtonType = 'default-destructive' | 'brand' | 'neutral' | 'outline' | 'destructive' | 'outline-destructive';
+type ButtonType =
+  | 'default-destructive'
+  | 'brand'
+  | 'neutral'
+  | 'outline'
+  | 'destructive'
+  | 'outline-destructive';
 
-const Button = ({ children, type, name, className, disabled, loading }: ButtonProps) => (
-  <button disabled={disabled} name={name} className={classnames({
-    'fd-button': true,
-    [`fd-button-${type}`]: type,
-    'fd-button-loading': loading,
-    [className as string]: className
-  })}>
-    {loading && <div className="fd-button__loader-container">
-    <CircleLoader size="2rem"/>
-      </div>}
+const Button = ({
+  children,
+  type,
+  name,
+  className,
+  disabled,
+  loading,
+}: ButtonProps) => (
+  <button
+    disabled={disabled}
+    name={name}
+    className={classnames({
+      'fd-button': true,
+      [`fd-button-${type}`]: type,
+      'fd-button-loading': loading,
+      [className as string]: className,
+    })}
+  >
+    {loading && (
+      <div className="fd-button__loader-container">
+        <CircleLoader size="2rem" />
+      </div>
+    )}
     <span className="fd-button__value">{children}</span>
   </button>
-)
+);
 
 export default Button;
