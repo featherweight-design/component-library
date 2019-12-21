8990a55d9fbfade98a1925d30eed0aba9c9ddd6d
Hugh Hartigan <Hartigan.HM@gmail.com>
2019-12-21 11:32:23 -0700

    Add Button type stories

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -6,6 +6,14 @@ import Button from './Button';
 
 storiesOf('Button', module)
   .addDecorator(checkA11y)
-  .add('Default', () => (
-    <Button>Click Me!</Button>
+  .add('Types', () => (
+    <div>
+      <Button>Default</Button>
+      <Button type="default-destructive">Default Destructive</Button>
+      <Button type="brand">Brand</Button>
+      <Button type="neutral">Neutral</Button>
+      <Button type="outline">Outline</Button>
+      <Button type="destructive">Destructive</Button>
+      <Button type="outline-destructive">Outline Destructive</Button>
+    </div>
   ));
