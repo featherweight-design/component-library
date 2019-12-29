c2608f966342dccbffdcd586ce82c4ef946738a5
Hugh Hartigan <Hartigan.HM@gmail.com>
2019-12-28 18:14:25 -0700

    Builds initial Radio component

---
+++ src/components/Radio/Radio.tsx
@@ -0,0 +1,31 @@
+import React, { ChangeEvent } from 'react';
+import classnames from 'classnames';
+
+import './Radio.scss';
+
+type RadioProps = {
+  options: string[];
+  selected: string;
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+};
+
+const Radio = ({ options, selected, onChange }: RadioProps) => (
+  <div className="fd-radio">
+    {options.map(option => (
+      <label key={option} className="fd-radio__container">
+        <input
+          type="radio"
+          className={classnames({
+            'fd-radio__input': true,
+          })}
+          name={option}
+          onChange={onChange}
+          checked={option === selected}
+        />
+        <div className="fd-radio__styled">{option}</div>
+      </label>
+    ))}
+  </div>
+);
+
+export default Radio;
