0272e1f1dd7414fc7ea25ac5648cc521730fa060
Hugh Hartigan <Hartigan.HM@gmail.com>
2019-12-28 17:21:08 -0700

    Adds disabled props for Checkbox

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -11,23 +11,32 @@ type CheckboxProps = {
 type CheckboxOption = {
   checked: boolean;
   label: string;
+  disabled?: boolean;
 };
 
 const Checkbox = ({ onChange, options }: CheckboxProps) => (
   <div className="fd-checkbox">
-    {options.map(({ label, checked }) => (
-      <label key={`fd-checkbox__${label}`} className="fd-checkbox__container">
+    {options.map(({ label, checked, disabled }) => (
+      <label
+        key={`fd-checkbox__${label}`}
+        className={classnames({
+          'fd-checkbox__container': true,
+          'fd-checkbox__container-disabled': disabled,
+        })}
+      >
         <input
           className="fd-checkbox__input"
           type="checkbox"
           name={label}
           checked={checked}
           onChange={onChange}
+          disabled={disabled}
         />
         <div
           className={classnames({
             'fd-checkbox__styled': true,
             'fd-checkbox__styled-checked': checked,
+            'fd-checkbox__styled-disabled': disabled,
           })}
         >
           {checked && <div className="fd-checkbox__icon" />}
