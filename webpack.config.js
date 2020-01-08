7522395e87f2ee624f21bc69558aa74ec22ff733
Hugh Hartigan <Hartigan.HM@gmail.com>
2020-01-07 22:09:09 -0700

    Resolves npm build issues; bumps to version 0.1.18

--- webpack.config.js
+++ webpack.config.js
@@ -38,7 +38,7 @@ module.exports = {
     rules: [
       {
         test: /\.tsx?$/,
-        loader: 'awesome-typescript-loader',
+        loader: 'ts-loader',
         exclude: /node_modules/,
       },
       {
