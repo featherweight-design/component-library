9b8991a39ce3fbb44d8f87b7b010d0b56f17b152
Hugh Hartigan <Hartigan.HM@gmail.com>
2020-05-27 18:50:30 -0600

    chore: Add type-enum to commitlint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,5 +2,22 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'subject-case': [2, 'always', ['sentence-case', 'start-case']],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'fix',
+        'feat',
+        'BREAKING CHANGE',
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'style',
+        'refactor',
+        'test',
+        'optimization',
+      ],
+    ],
   },
 };
