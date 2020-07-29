31a1731ee92ff5284e6b600b6ca57a74541271ff
Hugh Hartigan <Hartigan.HM@gmail.com>
2020-07-29 10:29:03 -0600

    chore: Add featherTheme

---
+++ .storybook/featherTheme.js
@@ -0,0 +1,12 @@
+import { create } from '@storybook/theming/create';
+
+const featherTheme = create({
+  base: 'dark',
+  brandImage: '/fwd-icon_32x32.png',
+  brandTitle: 'Featherweight Design Component Library',
+  brandUrl:
+    '(https://github.com/featherweight-design/component-library',
+  fontBase: '"Open Sans", Arial, sans-serif',
+});
+
+export default featherTheme;
