11accb5e3dfc440746257570adc7a0057ded4989
Hugh Hartigan <Hartigan.HM@gmail.com>
2020-07-29 10:26:36 -0600

    chore: Update storybook/config.ts

--- .storybook/config.ts
+++ .storybook/config.ts
@@ -1,5 +1,34 @@
-import { configure } from '@storybook/react';
-import '../src/styles/main.scss';
+import { configure, addParameters } from '@storybook/react';
+import './main.scss';
 
-// automatically import all files ending in *.stories.js
-configure(require.context('../src', true, /\.stories\.tsx$/), module);
+addParameters({
+  options: {
+    showRoots: true,
+    // Inspiration for sorting solution credit to Reaviz
+    // https://github.com/reaviz/reaviz/blob/master/.storybook/config.js
+    storySort: (a: any, b: any) => {
+      const idA = a[1].id;
+      const idB = b[1].id;
+      if (idA.includes('docs') || idB.includes('docs')) {
+        if (idA.includes('intro') || idB.includes('intro')) {
+          return 1;
+        }
+        return 0;
+      }
+      if (idA.includes('readme')) {
+        return -1;
+      }
+      return idA > idB ? 1 : -1;
+    },
+  },
+});
+
+const loadStories = () => {
+  return [
+    require.context('../docs', true, /\.stories.mdx/),
+    // automatically import all files ending in *.stories.tsx or *.stories.mdx
+    require.context('../src/components', true, /\.stories\.(ts|md)x$/),
+  ];
+};
+
+configure(loadStories, module);
