d13a19bbf50fea867574beee2dc899a54fc26c41
Hugh Hartigan <Hartigan.HM@gmail.com>
2020-07-30 19:02:11 -0600

    chore: Add Icon types

--- src/types.ts
+++ src/types.ts
@@ -1,35 +1,6 @@
 import { ChangeEvent } from 'react';
 
-// FORM
-export interface SelectOptionType {
-  value: any /*eslint-disable-line @typescript-eslint/no-explicit-any*/;
-  label: string;
-}
-
-export interface OtherOptionType {
-  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
-  value: string;
-}
-
-// NAVIGATION
-export interface CurrentlyViewingTab {
-  label: string;
-  path: string;
-  disabled: boolean;
-}
-
-export interface CurrentlyViewing {
-  path: string;
-  title: string;
-  subTitle?: string;
-  backPath?: string;
-  backTitle?: string;
-  tabs?: {
-    currentTab: string;
-    tabs: CurrentlyViewingTab[];
-  };
-}
-
+//* HeaderMenu
 export interface HeaderMenuOptions {
   [key: string]: HeaderMenuOption;
 }
@@ -51,6 +22,54 @@ export interface HeaderMenuSubOption {
   href?: string;
 }
 
+//* Icon Types
+export interface IconProps {
+  icon: string;
+  className?: string;
+  color?: string;
+  dataId?: string;
+  id?: string;
+  onClick?: () => void;
+  size?: string;
+}
+
+export interface IconArgsType {
+  icon: string;
+  className?: string;
+  color?: string;
+}
+
+//* Navigation
+export interface CurrentlyViewingTab {
+  label: string;
+  path: string;
+  disabled: boolean;
+}
+
+export interface CurrentlyViewing {
+  path: string;
+  title: string;
+  subTitle?: string;
+  backPath?: string;
+  backTitle?: string;
+  tabs?: {
+    currentTab: string;
+    tabs: CurrentlyViewingTab[];
+  };
+}
+
+//* Select Types
+export interface SelectOptionType {
+  value: any /*eslint-disable-line @typescript-eslint/no-explicit-any*/;
+  label: string;
+}
+
+export interface OtherOptionType {
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  value: string;
+}
+
+//* SideNavigation
 export interface SideNavigationOption {
   label: string;
   icon: string;
