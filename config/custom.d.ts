137cf8354b85bf46129c6e09fd4b97f32e9d51ac
Hugh Hartigan <Hartigan.HM@gmail.com>
2020-07-30 18:18:18 -0600

    chore: Build custom.d.ts for svg types

---
+++ config/custom.d.ts
@@ -0,0 +1,7 @@
+// Workaround for importing SVG files
+// Resource: https://webpack.js.org/guides/typescript/#importing-other-assets
+
+declare module '*.svg' {
+  const content: string;
+  export default content;
+}
