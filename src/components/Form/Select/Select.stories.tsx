fb00dddafd68ea90251dce244583af64013515b0
Hugh Hartigan <Hartigan.HM@gmail.com>
2021-01-10 11:27:22 -0700

    refactor: Update Select stories for new standards

--- src/components/Form/Select/Select.stories.tsx
+++ src/components/Form/Select/Select.stories.tsx
@@ -1,64 +1,91 @@
 import React, { useState } from 'react';
 import { withA11y } from '@storybook/addon-a11y';
-import { storiesOf } from '@storybook/react';
 
+import { SelectOptionType } from 'types';
 import Select from './Select';
-import { SelectOptionType } from '../../types';
 
-storiesOf('Select', module)
-  .addDecorator(withA11y)
-  .add('Default', () => {
-    const [selected, handleSelectOption] = useState<
-      SelectOptionType | undefined
-    >(undefined);
+export default {
+  component: 'Components/Form/Select',
+  decorators: [withA11y],
+};
 
-    const mockOnClick = (option: SelectOptionType): void => {
-      console.log({ option });
-      handleSelectOption(option);
-    };
+export const Default = (): JSX.Element => {
+  const [selected, handleSelectOption] = useState<SelectOptionType | undefined>(
+    undefined
+  );
 
-    return (
-      <div className="story__select-container">
-        <Select
-          label="Label"
-          selected={selected}
-          onSelect={mockOnClick}
-          options={[
-            { value: 'huey', label: 'Huhu' },
-            { value: 'juliana', label: 'Juju' },
-            { value: 'torin', label: 'Tor-bear' },
-            { value: 'juniper', label: 'Jean-june' },
-          ]}
-        />
+  const mockOnSelect = (option: SelectOptionType): void => {
+    console.log({ option });
+    handleSelectOption(option);
+  };
 
-        <Select
-          disabled
-          label="Disabled"
-          selected={{
-            value: 'disabled',
-            label: 'Locked',
-          }}
-          onSelect={mockOnClick}
-          options={[
-            { value: 'huey', label: 'Huhu' },
-            { value: 'juliana', label: 'Juju' },
-            { value: 'torin', label: 'Tor-bear' },
-            { value: 'juniper', label: 'Jean-june' },
-          ]}
-        />
+  return (
+    <Select
+      label="Favorite Creature"
+      selected={selected}
+      onSelect={mockOnSelect}
+      options={[
+        { value: 'alien', label: 'Alien 👽' },
+        { value: 'dragon', label: 'Dragon 🐉' },
+        { value: 'ghost', label: 'Ghost 👻' },
+        { value: 'unicorn', label: 'Unicorn 🦄' },
+      ]}
+    />
+  );
+};
 
-        <Select
-          label="Label"
-          selected={selected}
-          onSelect={mockOnClick}
-          errorMessage="Please pick a Hartigan"
-          options={[
-            { value: 'huey', label: 'Huhu' },
-            { value: 'juliana', label: 'Juju' },
-            { value: 'torin', label: 'Tor-bear' },
-            { value: 'juniper', label: 'Jean-june' },
-          ]}
-        />
-      </div>
-    );
-  });
+export const Types = (): JSX.Element => {
+  const [selected, handleSelectOption] = useState<SelectOptionType | undefined>(
+    undefined
+  );
+
+  const mockOnSelect = (option: SelectOptionType): void => {
+    console.log({ option });
+    handleSelectOption(option);
+  };
+
+  return (
+    <div className="story__select-container">
+      <Select
+        label="Label"
+        selected={selected}
+        onSelect={mockOnSelect}
+        options={[
+          { value: 'alien', label: 'Alien 👽' },
+          { value: 'dragon', label: 'Dragon 🐉' },
+          { value: 'ghost', label: 'Ghost 👻' },
+          { value: 'unicorn', label: 'Unicorn 🦄' },
+        ]}
+      />
+
+      <Select
+        disabled
+        label="Disabled"
+        selected={{
+          value: 'disabled',
+          label: 'Locked',
+        }}
+        onSelect={mockOnSelect}
+        options={[
+          { value: 'alien', label: 'Alien 👽' },
+          { value: 'dragon', label: 'Dragon 🐉' },
+          { value: 'ghost', label: 'Ghost 👻' },
+          { value: 'unicorn', label: 'Unicorn 🦄' },
+        ]}
+      />
+
+      <Select
+        label="Label"
+        selected={selected}
+        onSelect={mockOnSelect}
+        errorMessage="Please pick a Hartigan"
+        options={[
+          { value: 'alien', label: 'Alien 👽' },
+          { value: 'dragon', label: 'Dragon 🐉' },
+          { value: 'ghost', label: 'Ghost 👻' },
+          { value: 'unicorn', label: 'Unicorn 🦄' },
+        ]}
+      />
+    </div>
+  );
+};
