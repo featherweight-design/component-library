ede3b9eb655c76b3e84d7a627f8d173c42535ad5
Hugh Hartigan <Hartigan.HM@gmail.com>
2021-01-10 08:14:20 -0700

    refactor: Update CircleLoader stories to new standards

--- src/components/Loaders/CircleLoader/CircleLoader.stories.tsx
+++ src/components/Loaders/CircleLoader/CircleLoader.stories.tsx
@@ -1,11 +1,17 @@
 import * as React from 'react';
-import { checkA11y } from '@storybook/addon-a11y';
-import { storiesOf } from '@storybook/react';
+import { withA11y } from '@storybook/addon-a11y';
 
 import CircleLoader from './CircleLoader';
 
-storiesOf('Loaders/Circle Loader', module)
-  .addDecorator(checkA11y)
-  .add('Default', () => <CircleLoader />)
-  .add('Custom color', () => <CircleLoader color="magenta" />)
-  .add('Custom size', () => <CircleLoader size="250" />);
+export default {
+  title: 'Components/Loaders/CircleLoader',
+  decorators: [withA11y],
+};
+
+export const Default = (): JSX.Element => <CircleLoader />;
+
+export const WithCustomColor = (): JSX.Element => (
+  <CircleLoader color="magenta" />
+);
+
+export const WithCustomSize = (): JSX.Element => <CircleLoader size="250" />;
