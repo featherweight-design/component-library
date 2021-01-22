46c3ff00f8921f041b6eaebdcc6ba20771221ba0
Hugh Hartigan <Hartigan.HM@gmail.com>
2021-01-22 11:22:18 -0700

    build: Add structure for copyContent

---
+++ src/shared/data/copyContent/buttonCopy.ts
@@ -0,0 +1,13 @@
+export default {
+  brand: 'Brand',
+  default: 'Default',
+  defaultDestructive: 'Default Destructive',
+  destructive: 'Destructive',
+  glass: 'Glass',
+  neutral: 'Neutral',
+  outline: 'Outline',
+  outlineDestructive: 'Outline Destructive',
+  square: 'Square',
+  round: 'Round',
+  roundedSquare: 'Rounded Square (Default)',
+};
