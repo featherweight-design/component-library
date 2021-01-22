41995f7a3bae2cb1b482c8a77cbc4772fcbdf1b1
Hugh Hartigan <Hartigan.HM@gmail.com>
2021-01-22 12:03:46 -0700

    test: Write initial Button Cypress tests

--- src/cypress/integration/Buttons/Button/Button.spec.ts
+++ src/cypress/integration/Buttons/Button/Button.spec.ts
@@ -5,6 +5,13 @@ describe('Button tests', () => {
     cy.visitStorybook();
   });
 
+  beforeEach(() => {
+    // The first parameter is the category. This is the `title` in CSF or the value in `storiesOf`
+    // The second parameter is the name of the story. This is the name of the function in CSF or the value in the `add`
+    // This does not refresh the page, but will unmount any previous story and use the Storybook Router API to render a fresh new story
+    cy.loadStory('Button', 'Text');
+  });
+
   // describe('Playground', () => {});
 
   // describe('Shapes', () => {});
