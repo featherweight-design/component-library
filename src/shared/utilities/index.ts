66eb532dabdedf266c2e3c10d5a3d7a6e2ad9ae1
Hugh Hartigan <Hartigan.HM@gmail.com>
2021-01-22 10:37:59 -0700

    chore: Build /shared directory; migrate /utilities

---
+++ src/shared/utilities/index.ts
@@ -0,0 +1,5 @@
+import generateColorContrast from './generateColorContrast';
+
+// Remove when more exports are added
+/* eslint-disable-next-line import/prefer-default-export */
+export { generateColorContrast };
