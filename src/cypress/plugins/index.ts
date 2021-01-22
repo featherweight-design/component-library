fe185d8ccc25d932fe39d29d90d9fd20561199a1
Hugh Hartigan <Hartigan.HM@gmail.com>
2021-01-22 11:04:42 -0700

    chore: Update Cypress plugin/support files

--- src/cypress/plugins/index.ts
+++ src/cypress/plugins/index.ts
@@ -1,20 +1,24 @@
+/// <reference types="cypress" />
 // ***********************************************************
-// This example support/index.js is processed and
-// loaded automatically before your test files.
+// This example plugins/index.js can be used to load plugins
 //
-// This is a great place to put global configuration and
-// behavior that modifies Cypress.
-//
-// You can change the location of this file or turn off
-// automatically serving support files with the
-// 'supportFile' configuration option.
+// You can change the location of this file or turn off loading
+// the plugins file with the 'pluginsFile' configuration option.
 //
 // You can read more here:
-// https://on.cypress.io/configuration
+// https://on.cypress.io/plugins-guide
 // ***********************************************************
 
-// Import commands.js using ES2015 syntax:
-import './commands';
+// This function is called when a project is opened or re-opened (e.g. due to
+// the project's config changing)
 
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
+/**
+ * @type {Cypress.PluginConfig}
+ */
+
+// Remove the eslint-disable once this is being used
+/* eslint-disable-next-line @typescript-eslint/no-unused-vars */
+// module.exports = (on, config) => {
+//   // `on` is used to hook into various events Cypress emits
+//   // `config` is the resolved Cypress config
+// };
