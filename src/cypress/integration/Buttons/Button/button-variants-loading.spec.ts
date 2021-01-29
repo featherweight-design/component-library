b718f8ab757fc4db2856e5da666bfd4ea0ef2da5
Hugh Hartigan <Hartigan.HM@gmail.com>
2021-01-29 09:19:10 -0700

    test: Write Button variants loading tests

---
+++ src/cypress/integration/Buttons/Button/button-variants-loading.spec.ts
@@ -0,0 +1,56 @@
+import { buttonCopy } from 'shared/data/copyContent';
+
+describe('Button Variants tests', () => {
+  before(() => {
+    cy.visitStorybook();
+    cy.loadStory('components-buttons-button', 'variants-loading');
+  });
+
+  it('Should have a "Default" button', () => {
+    cy.findByText(buttonCopy.default)
+      .closest('button')
+      .find('.fd-circle-loader');
+  });
+
+  it('Should have a "Default Destructive" button', () => {
+    cy.findByText(buttonCopy.defaultDestructive)
+      .closest('button')
+      .find('.fd-circle-loader');
+  });
+
+  it('Should have a "Brand" button', () => {
+    cy.findByText(buttonCopy.brand)
+      .closest('button')
+      .find('.fd-circle-loader');
+  });
+
+  it('Should have a "Neutral" button', () => {
+    cy.findByText(buttonCopy.neutral)
+      .closest('button')
+      .find('.fd-circle-loader');
+  });
+
+  it('Should have a "Outline" button', () => {
+    cy.findByText(buttonCopy.outline)
+      .closest('button')
+      .find('.fd-circle-loader');
+  });
+
+  it('Should have a "Destructive" button', () => {
+    cy.findByText(buttonCopy.destructive)
+      .closest('button')
+      .find('.fd-circle-loader');
+  });
+
+  it('Should have a "Outline Destructive" button', () => {
+    cy.findByText(buttonCopy.outlineDestructive)
+      .closest('button')
+      .find('.fd-circle-loader');
+  });
+
+  it('Should have a "Glass" button', () => {
+    cy.findByText(buttonCopy.glass)
+      .closest('button')
+      .find('.fd-circle-loader');
+  });
+});
