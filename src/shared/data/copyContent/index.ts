134ea48dfc7cf1b22b4b0730a0fd9df54effd9bc
Hugh Hartigan <Hartigan.HM@gmail.com>
2021-01-30 13:09:56 -0700

    chore: Add actionButtonCopy

--- src/shared/data/copyContent/index.ts
+++ src/shared/data/copyContent/index.ts
@@ -1,5 +1,4 @@
+import actionButtonCopy from './actionButtonCopy';
 import buttonCopy from './buttonCopy';
 
-// Remove when more exports are added
-/* eslint-disable-next-line import/prefer-default-export */
-export { buttonCopy };
+export { actionButtonCopy, buttonCopy };
