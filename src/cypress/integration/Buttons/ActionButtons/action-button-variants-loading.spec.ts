3cccf31262ebd88e045504c10459d37b858fa090
Hugh Hartigan <Hartigan.HM@gmail.com>
2021-01-30 17:25:27 -0700

    test: Write ActionButton variants loading tests

---
+++ src/cypress/integration/Buttons/ActionButtons/action-button-variants-loading.spec.ts
@@ -0,0 +1,26 @@
+import { actionButtonCopy } from 'shared/data/copyContent';
+
+describe('ActionButton Variants tests', () => {
+  before(() => {
+    cy.visitStorybook();
+    cy.loadStory('components-buttons-actionbutton', 'variants');
+  });
+
+  it('Should have a "Primary" button', () => {
+    cy.findByText(actionButtonCopy.primary)
+      .closest('button')
+      .find('.fd-circle-loader');
+  });
+
+  it('Should have a "Secondary" button', () => {
+    cy.findByText(actionButtonCopy.secondary)
+      .closest('button')
+      .find('.fd-circle-loader');
+  });
+
+  it('Should have a "Glass" button', () => {
+    cy.findByText(actionButtonCopy.glass)
+      .closest('button')
+      .find('.fd-circle-loader');
+  });
+});
