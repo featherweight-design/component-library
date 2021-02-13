104757967b2bc52d0baed61d5a5941bcf14865fa
Hugh Hartigan <Hartigan.HM@gmail.com>
2021-02-13 14:58:14 -0700

    chore: Write headerMenuCopy

---
+++ src/shared/data/copyContent/headerMenuCopy.ts
@@ -0,0 +1,20 @@
+export default {
+  defaultTitle: 'Best Site',
+  defaultSubTitle: 'ever',
+  icons: {
+    info: 'info',
+    notifications: 'notifications',
+    permissions: 'account_tree',
+    search: 'search',
+    settings: 'settings',
+    support: 'mail',
+    userManagement: 'group',
+  },
+  subOptions: {
+    infoLabel: 'Info',
+    permissionsLabel: 'Permissions',
+    supportLabel: 'Support',
+    userManagementLabel: 'User Management',
+  },
+  userName: 'Bob H.',
+};
--- src/shared/data/copyContent/index.ts
+++ src/shared/data/copyContent/index.ts
@@ -1,5 +1,6 @@
 import actionButtonCopy from './actionButtonCopy';
 import buttonCopy from './buttonCopy';
+import headerMenuCopy from './headerMenuCopy';
 import inputCopy from './inputCopy';
 
-export { actionButtonCopy, buttonCopy, inputCopy };
+export { actionButtonCopy, buttonCopy, headerMenuCopy, inputCopy };
