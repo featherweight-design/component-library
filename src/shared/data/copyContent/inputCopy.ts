c55278ea7c54e27787b78c41d80696aa0758d2a5
Hugh Hartigan <Hartigan.HM@gmail.com>
2021-02-20 16:16:51 -0700

    chore: Add icons to inputCopy

--- src/shared/data/copyContent/inputCopy.ts
+++ src/shared/data/copyContent/inputCopy.ts
@@ -2,6 +2,10 @@ export default {
   disabledLabel: 'Disabled',
   errorLabel: 'Error',
   errorMessage: 'Please enter a value',
+  icons: {
+    showPassword: 'visibility',
+    hidePassword: 'visibility_off',
+  },
   minimalLabel: 'Minimal variant',
   numberLabel: 'Number',
   numberPlaceholder: '%',
