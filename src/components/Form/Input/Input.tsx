cc64384e13dbb8917008432eda582bcd6fd29b8a
Hugh Hartigan <Hartigan.HM@gmail.com>
2021-02-20 11:20:14 -0700

    feat: Add password show/hide functionality to Input component

--- src/components/Form/Input/Input.tsx
+++ src/components/Form/Input/Input.tsx
@@ -1,14 +1,17 @@
-import { FC } from 'react';
+import { useState, FC } from 'react';
 import classnames from 'classnames';
 
+import Icon from 'components/Icon/Icon';
 import { InputProps } from 'types';
+
+const DEFAULT_INPUT_TYPE = 'text';
 
 const Input: FC<InputProps> = ({
   name,
   value,
   onChange,
   id,
-  type,
+  type = DEFAULT_INPUT_TYPE,
   label,
   placeholder,
   min,
@@ -16,37 +19,62 @@ const Input: FC<InputProps> = ({
   disabled,
   className,
   errorMessage,
-}: InputProps) => (
-  <div
-    className={classnames({
-      'fd-input': true,
-      [className as string]: className,
-    })}
-  >
-    {label && (
-      <label htmlFor={id || `fd-input__${name}`} className="fd-label">
-        {label}
-      </label>
-    )}
-    <input
-      id={id || `fd-input__${name}`}
-      name={name}
-      value={value}
-      type={type}
-      placeholder={placeholder}
-      onChange={onChange}
-      min={min}
-      max={max}
-      disabled={disabled}
-      className={classnames({
-        'fd-input__input': true,
-        'fd-input__input-error': errorMessage,
-      })}
-    />
+}: InputProps) => {
+  const [isPasswordShown, handleToggleShowPassword] = useState(false);
 
-    <p className="fd-input-error">{errorMessage}</p>
-  </div>
-);
+  const getInputType = (): string => {
+    if (type === 'password' && isPasswordShown) {
+      return DEFAULT_INPUT_TYPE;
+    }
+
+    return type;
+  };
+
+  return (
+    <div
+      className={classnames({
+        'fd-input': true,
+        [className as string]: className,
+      })}
+    >
+      {label && (
+        <label
+          htmlFor={id || `fd-input__${name}`}
+          className={classnames(['fd-label', 'fd-input__label'])}
+        >
+          {label}
+        </label>
+      )}
+      <input
+        id={id || `fd-input__${name}`}
+        name={name}
+        value={value}
+        type={getInputType()}
+        placeholder={placeholder}
+        onChange={onChange}
+        min={min}
+        max={max}
+        disabled={disabled}
+        className={classnames({
+          'fd-input__input': true,
+          'fd-input__input-error': errorMessage,
+        })}
+      />
+
+      {type === 'password' && (
+        <button
+          className="fd-input__password-button"
+          type="button"
+          onClick={(): void => handleToggleShowPassword(!isPasswordShown)}
+        >
+          <Icon icon={isPasswordShown ? 'visibility_off' : 'visibility'} />
+        </button>
+      )}
+
+      <p className="fd-input-error">{errorMessage}</p>
+    </div>
+  );
+};
 
 Input.defaultProps = {
   id: '',
