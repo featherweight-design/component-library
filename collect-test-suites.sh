70444563bd5053f44267c6eaeb67884235692251
Hugh Hartigan <Hartigan.HM@gmail.com>
2021-02-24 21:39:03 -0700

    chore: Build collect-test-suites.sh

--- collect-test-suites.sh
+++ collect-test-suites.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-SPEC_FILE_ARRAY=find ./src/cypress -type f -name '*.spec.ts'
+SPEC_FILE_ARRAY=($(find ./src/cypress/ -name "*.spec.ts"))
+TEST_SUITE_ARRAY=()
 
-echo $SPEC_FILE_ARRAY
+for i in "${SPEC_FILE_ARRAY[@]}"
+do
+  TEST_SUITE_ARRAY+=($(basename "$(dirname "$i")"))
+done
+
+UNIQUE_TEST_SUITES=$(echo "${TEST_SUITE_ARRAY[@]}" | tr ' ' '\n' | sort -u | tr '\n' ',')
+echo $UNIQUE_TEST_SUITES
