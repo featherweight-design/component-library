676a766dd4307f881d1b0e61f6ad96a43d7d1866
Hugh Hartigan <Hartigan.HM@gmail.com>
2021-02-24 19:25:38 -0700

    chore: Move cy.wait to correst test suite

--- src/cypress/integration/Navigation/HeaderMenu/header-menu-with-sub-options.spec.ts
+++ src/cypress/integration/Navigation/HeaderMenu/header-menu-with-sub-options.spec.ts
@@ -32,15 +32,15 @@ describe('HeaderMenu with Sub Options tests', () => {
 
   it('Should close the menu when clicking the settings icon', () => {
     cy.findByText(headerMenuCopy.icons.settings).click();
-    // TODO: Figure out a solution that does not use cy.wait
-    // Wait here needed for menu animation to complete before clicking away
-    /* eslint-disable-next-line cypress/no-unnecessary-waiting */
-    cy.wait(200);
     cy.findByRole('menu').should('not.be.visible');
   });
 
   it('Should close the menu when clicking off the menu', () => {
     cy.findByText(headerMenuCopy.icons.settings).click();
+    // TODO: Figure out a solution that does not use cy.wait
+    // Wait here needed for menu animation to complete before clicking away
+    /* eslint-disable-next-line cypress/no-unnecessary-waiting */
+    cy.wait(200);
     cy.findByRole('menubar').click();
     cy.findByRole('menu').should('not.be.visible');
   });
