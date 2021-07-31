c204f0ff6e1fdf9801a5d3002b2b4ad31dfe346f
Hugh Hartigan <Hartigan.HM@gmail.com>
2021-07-31 08:51:27 -0600

    chore: Undo changes to Buttons

--- src/components/Buttons/Button/Button.stories.tsx
+++ src/components/Buttons/Button/Button.stories.tsx
@@ -1,6 +1,5 @@
 import { useState, ChangeEvent } from 'react';
 import { withA11y } from '@storybook/addon-a11y';
-import { Meta } from '@storybook/react';
 
 import { Input } from 'components/Form';
 import { buttonCopy } from 'shared/data/copyContent';
@@ -9,7 +8,7 @@ import Button from './Button';
 export default {
   title: 'Components/Buttons/Button',
   decorators: [withA11y],
-} as Meta;
+};
 
 /* eslint-disable-next-line no-console */
 const mockClick = (): void => console.log("I've been clicked!");
